41e82a0e9e21ff2b344553e8a52ba42e9b25153a
Cooper van Wijck <cooper@vanwijck.me>
2026-02-18 22:55:46 +1100

    Update bio

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import { BlogPosts } from 'app/components/posts'
-import TypeItComponent from 'app/components/typed'
+import { BlogPosts } from "app/components/posts";
+import TypeItComponent from "app/components/typed";
 
 export const metadata = {
-  title: 'Home | Cooper van Wijck', // This will use the template to become "Home | Cooper van Wijck"
-  description: 'The personal website of Cooper van Wijck, a final-year student at the University of New South Wales studying Law and Computer Science.',
+  title: "Home | Cooper van Wijck", // This will use the template to become "Home | Cooper van Wijck"
+  description:
+    "The personal website of Cooper van Wijck, AI Engineer and Team Lead at HUB24. Explore insights on AI, technology, and practical solutions at the intersection of law and innovation.",
 };
 
 export default function Page() {
@@ -13,20 +14,25 @@ export default function Page() {
         <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-semibold tracking-tighter whitespace-nowrap">
           Hi my name&apos;s&nbsp;
         </h1>
-        <TypeItComponent speed={200} className='font-semibold text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-mono tracking-tighter'>Cooper.</TypeItComponent>
+        <TypeItComponent
+          speed={200}
+          className="font-semibold text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-mono tracking-tighter"
+        >
+          Cooper.
+        </TypeItComponent>
       </div>
       <p className="my-4">
-        {`I'm a technology focused graduate at `}
-        <a href="https://hub24.com.au" className="text-blue-300 underline">HUB24</a>
-        {`, I apply my degrees in Law and Computer Science to dissect and apply solutions to emerging fields. My passion lies at the intersection of technology and law, and I'm excited to explore how new technologies impact and shape society. I enjoy investigating how automation and the integration of technology can enhance efficiency and enjoyment in our daily lives.
+        {`As an AI Engineer and Team Lead at `}
+        <a href="https://hub24.com.au" className="text-blue-300 underline">
+          HUB24
+        </a>
+        {`, I apply my knowledge across law and technology to build practical and compliant solutions. I work across the technology stack to combine infrastructure, application development and business understanding to build genuinely useful software. I'm always excited to experiment and explore how technology solutions can reduce tedious tasks so we can focus on the important things.
         `}
       </p>
-      <h3 className="mb-2 text-4xl font-semibold tracking-tighter">
-        Blog
-      </h3>
+      <h3 className="mb-2 text-4xl font-semibold tracking-tighter">Blog</h3>
       <div className="items-start justify-start">
         <BlogPosts />
       </div>
     </section>
-  )
+  );
 }
